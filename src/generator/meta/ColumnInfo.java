9e1b9b93892fd2b384cbe9702e464e116cb1972e
zhangjian <jianzhang@sendcloud.im>
2015-07-13 16:15:22 +0800

    tests

---
+++ src/generator/meta/ColumnInfo.java
@@ -0,0 +1,13 @@
+package generator.meta;
+
+public enum ColumnInfo {
+	UNKNOWN();
+	private String feildName;
+	private String columnName;
+	private Integer columnIndex;
+	private String dataType;
+	private String columnType;
+	private String columnComment;
+	
+	
+}
